847c9a842503759a70b5c71e2d80ce58c1bcdab8
anas1993 <shaikhanas1993@gmail.com>
2019-04-25 23:15:52 +0530

    c# practice programm

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+
+// namespace c_
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+// using System;
+
+// namespace c_{
+//     class Program{
+//         public static void Main(string[] args){
+//             Console.WriteLine("Hello world!!");
+//         }
+//     }
+// }
+
+
+using System;
+namespace c_{
+    class Program {
+        static void Main(string[] args) => Console.WriteLine("Hello world");
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,7 @@
+namespace c_{
+    class Test1{
+        // public static void Main(string[] args){
+        //     System.Console.WriteLine("hekki wrdsf");
+        // }
+    }
+}
