73e0226aac1a29e1c52aa42b17ade61e397296ae
anas1993 <shaikhanas1993@gmail.com>
2019-04-26 11:50:20 +0530

    minor changes

--- Program.cs
+++ Program.cs
@@ -2,10 +2,25 @@
 using Services;
 namespace c_{
     class Program {
+        const double pi = 3.14159;   
         static void Main(string[] args) {
-            Rectangle obj = new Rectangle();
-            obj.PrintCalculatedArea();
-            var @try = "safaf";
+
+           
+           Console.WriteLine("Hello\tWorld\n\n");
+
+            // Rectangle obj = new Rectangle();
+            // obj.PrintCalculatedArea();
+            // var str = "hello world";
+            // System.Console.WriteLine("{0}",sizeof(double));
+
+            // double d = 124.1241414;
+            // int a  = (int) d;
+
+            //  str = d.ToString();
+            // System.Console.WriteLine(str.GetType() );
+            // System.Console.WriteLine("a :: {0}",a);
+            // System.Console.WriteLine(d.ToString());    
+
         }
     }
 }   
