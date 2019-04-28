c38652728a889e693f3dc095626e6f0bf51d2aeb
anas1993 <shaikhanas1993@gmail.com>
2019-04-28 19:48:57 +0530

    learn about upcasting and downcasting

--- Program.cs
+++ Program.cs
@@ -3,33 +3,79 @@ using Services;
 namespace c_
 {
 
-    class PrintData 
+    // class PrintData 
+    // {
+    //     public void print(int a){
+    //         System.Console.WriteLine(a);
+    //     }
+
+    //     public void print(float a){
+    //         System.Console.WriteLine(a);
+    //     }
+
+    //     public void print(double a){
+    //         System.Console.WriteLine(a);
+    //     }
+
+    //     public void print(string a){
+    //         System.Console.WriteLine(a);
+    //     }
+    // }
+
+    // abstract class Shape
+    // {
+    //     public abstract  int GetArea(int area);
+    // }
+
+    // class Rectangle: Shape{
+    //     public override int GetArea(int area){
+    //         return area * 10;
+    //     }
+    // }
+
+
+    // class Subject
+    // {
+    //     virtual 
+    // }
+
+    class Shape
     {
-        public void print(int a){
-            System.Console.WriteLine(a);
+        protected int width,height;
+
+        public Shape(int width,int height){
+            this.width = width;
+            this.height = height;
         }
 
-        public void print(float a){
-            System.Console.WriteLine(a);
+        public virtual int getArea(){
+            return 0;
         }
+    
+    
+    }
 
-        public void print(double a){
-            System.Console.WriteLine(a);
-        }
-
-        public void print(string a){
-            System.Console.WriteLine(a);
+    class Rectangle : Shape
+    {
+        public Rectangle(int width,int height):base(width,height){}
+        public override int getArea(){
+            return width * height;
         }
     }
 
-    abstract class Shape
+    class Triangle : Shape
     {
-        abstract public int GetArea(int area);
+        public Triangle(int width,int height):base(width,height){}
+        public override int getArea(){
+            return (width * height)/2;
+        }
     }
 
-    class Rectangle: Shape{
-        public override int GetArea(int area){
-            return area * 10;
+    class Caller
+    {
+        public void CallArea(Shape obj)
+        {
+            System.Console.WriteLine(obj.getArea())  ;
         }
     }
 
@@ -38,9 +84,16 @@ namespace c_
         static void Main(string[] args) 
         {
 
-           var obj = new Rectangle();
-           int sum = obj.GetArea(10);
-           Console.WriteLine(sum);
+            Caller obj = new Caller();
+            Rectangle rec = new Rectangle(10,10);
+            Triangle tri = new Triangle(10,10);
+
+            obj.CallArea(rec);
+            obj.CallArea(tri);
+
+        //    var obj = new Rectangle();
+        //    int sum = obj.GetArea(10);
+        //    Console.WriteLine(sum);
 
             // var obj = new PrintData();
             // obj.print(10);
