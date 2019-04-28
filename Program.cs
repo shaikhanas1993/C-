6b00c37ab59e2ae3b3f439c3c9c2e02243eae003
anas1993 <shaikhanas1993@gmail.com>
2019-04-28 11:25:44 +0530

    learn about function overloading in c#

--- Program.cs
+++ Program.cs
@@ -2,22 +2,35 @@
 using Services;
 namespace c_
 {
+
+    class PrintData 
+    {
+        public void print(int a){
+            System.Console.WriteLine(a);
+        }
+
+        public void print(float a){
+            System.Console.WriteLine(a);
+        }
+
+        public void print(double a){
+            System.Console.WriteLine(a);
+        }
+
+        public void print(string a){
+            System.Console.WriteLine(a);
+        }
+    }
+
     class Program 
     {
         static void Main(string[] args) 
         {
-            // Rec obj = new Rec(10,10);
-            // // double area = obj.getArea();
-            // // System.Console.WriteLine(area);
-            // Mas obj = new Mas(10,10);
-            // Console.WriteLine(obj.getCost());
-
-            TestP obj = new TestP();
-            obj.setWidth(10);
-            obj.setHeight(10);
-            int sum = obj.GetCost(10);
-            Console.WriteLine(sum);
-
+            
+            var obj = new PrintData();
+            obj.print(10);
+            obj.print(10.2);
+            obj.print("10");
         }
     }
 }
