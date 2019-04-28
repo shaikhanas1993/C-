2226bdb8a9531504207e50e6df29ed2b322ffaa9
anas1993 <shaikhanas1993@gmail.com>
2019-04-28 19:58:55 +0530

    learn about operator overloading in c#

--- Program.cs
+++ Program.cs
@@ -39,43 +39,58 @@ namespace c_
     //     virtual 
     // }
 
-    class Shape
-    {
-        protected int width,height;
+    // class Shape
+    // {
+    //     protected int width,height;
 
-        public Shape(int width,int height){
-            this.width = width;
-            this.height = height;
-        }
+    //     public Shape(int width,int height){
+    //         this.width = width;
+    //         this.height = height;
+    //     }
 
-        public virtual int getArea(){
-            return 0;
-        }
+    //     public virtual int getArea(){
+    //         return 0;
+    //     }
     
     
-    }
+    // }
 
-    class Rectangle : Shape
-    {
-        public Rectangle(int width,int height):base(width,height){}
-        public override int getArea(){
-            return width * height;
-        }
-    }
+    // class Rectangle : Shape
+    // {
+    //     public Rectangle(int width,int height):base(width,height){}
+    //     public override int getArea(){
+    //         return width * height;
+    //     }
+    // }
 
-    class Triangle : Shape
-    {
-        public Triangle(int width,int height):base(width,height){}
-        public override int getArea(){
-            return (width * height)/2;
-        }
-    }
+    // class Triangle : Shape
+    // {
+    //     public Triangle(int width,int height):base(width,height){}
+    //     public override int getArea(){
+    //         return (width * height)/2;
+    //     }
+    // }
 
-    class Caller
+    // class Caller
+    // {
+    //     public void CallArea(Shape obj)
+    //     {
+    //         System.Console.WriteLine(obj.getArea())  ;
+    //     }
+    // }
+
+
+    class Box
     {
-        public void CallArea(Shape obj)
-        {
-            System.Console.WriteLine(obj.getArea())  ;
+        public int width {get;set;}
+        public int height {get;set;}
+
+
+        public static Box operator + (Box b1,Box b2){
+            Box b = new Box();
+            b.height = b1.height + b2.height;
+            b.width = b1.width + b2.width;
+            return b;
         }
     }
 
@@ -83,13 +98,28 @@ namespace c_
     {
         static void Main(string[] args) 
         {
+            Box b1 = new Box();
+            b1.height = 10;
+            b1.width = 10;
 
-            Caller obj = new Caller();
-            Rectangle rec = new Rectangle(10,10);
-            Triangle tri = new Triangle(10,10);
+            Box b2 = new Box();
+            b2.height = 10;
+            b2.width = 10;
 
-            obj.CallArea(rec);
-            obj.CallArea(tri);
+            var obj = b1 + b2;
+            if(obj is Box)
+            {
+                System.Console.WriteLine(obj.ToString());
+            }
+
+
+
+            // Caller obj = new Caller();
+            // Rectangle rec = new Rectangle(10,10);
+            // Triangle tri = new Triangle(10,10);
+
+            // obj.CallArea(rec);
+            // obj.CallArea(tri);
 
         //    var obj = new Rectangle();
         //    int sum = obj.GetArea(10);
