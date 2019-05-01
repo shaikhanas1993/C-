292420e0040cc0eb1349f9141b51c57769ad3912
anas1993 <shaikhanas1993@gmail.com>
2019-05-01 13:35:29 +0530

    learn about xml comments in c#

--- Program.cs
+++ Program.cs
@@ -2,83 +2,22 @@
 using Services;
 namespace c_
 {
-    // public interface ITransaction
-    // {
 
-    //     void ShowTransaction();
-    //     double GetAmount();
-    // }
-
-    // class Test : ITransaction
-    // {
-
-    //     public int a {get;set;}
-    //     public int b {get;set;}
-
-    //     public Test()
-    //     {
-    //     }
-
-    //     public Test(int a, int b)
-    //     {
-    //         this.a = a;
-    //         this.b = b;
-    //     }
-    //     public double GetAmount()
-    //     {
-    //         return a*b;
-    //     }
-
-    //     public void ShowTransaction()
-    //     {
-    //         System.Console.WriteLine("a == {0} , b == {1}",a,b);
-    //     }
-    // }
-
-
-    namespace c1
-    {
-        class Add 
-        {
-            public int add(int a ,int b){
-                return a + b; 
-            }
-        }
-
-        namespace JSON
-        {
-            class Json
-            {
-                public void print()
-                {
-                    Console.WriteLine("in json class");
-                }
-            }
-        }
-    }
-
-     namespace c2
-    {
-        class Add 
-        {
-            public int add(int a ,int b){
-                return a + b + b; 
-            }
-        }
-    }
-
+    /// <summary>
+    /// hello world class
+    /// </summary>
     class Program 
     {
+
+        /// <summary>
+        ///  entry point 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args) 
         {
-            var obj1 = new c1.Add();
-            var obj2 = new c2.Add();
-            System.Console.WriteLine(obj1.add(5,6));
 
-            var obj3 = new c1.JSON.Json();
-            obj3.print();
+            
 
-            System.Console.WriteLine(obj2.add(5,6));      
         }
     }
 }
