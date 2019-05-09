9e98eddd8172aba27ae0ac5e746c286ffd7731ea
anas1993 <shaikhanas1993@gmail.com>
2019-05-09 12:06:05 +0530

    learning about events and delegates in c#

--- Program.cs
+++ Program.cs
@@ -3,31 +3,105 @@ using System.Collections.Generic;
 using Services;
 namespace c_
 {
-    public delegate void Print();
-
-    class Test
+    /// <summary>
+    /// Event Publisher Class
+    /// </summary>
+    class PrintHelper
     {
-        public void printNumber()
+        public delegate void BeforePrint();
+        public event BeforePrint beforePrintEvent;
+
+        public PrintHelper(){}
+
+        public void PrintNumber()
         {
-            Console.WriteLine("i am printing number");
+            if(beforePrintEvent != null)
+            {
+                beforePrintEvent();
+            }
+
+            Console.WriteLine("Printing Number");
         }
 
-        public void printZero()
+        public void PrintMoney()
         {
-            Console.WriteLine("i am printing zero");
+            if(beforePrintEvent != null)
+            {
+               
+                //Event publisher or raiser
+                beforePrintEvent.Invoke();
+            }
+
+            Console.WriteLine("printing Money");
         }
-    }
+
+        public void PrintCode()
+        {
+            if(beforePrintEvent != null)
+            {
+                beforePrintEvent.Invoke();
+            }
+
+            Console.WriteLine("Printing Code");
+        }
+   }
+    
+    
+
+    /// <summary>
+    ///    Event Subscriber 
+    /// </summary>
+   class Number
+   {
+       private PrintHelper _printHelper;
+       private int _value;
+
+       public Number(int value)
+       {
+           _value = value;
+           _printHelper =  new PrintHelper();
+          
+          
+           _printHelper.beforePrintEvent += beforePrintEventHandler;
+       } 
+
+       /**
+        *    Event Handler
+        */ 
+       public void beforePrintEventHandler()
+       {
+           Console.WriteLine("i am event handler");
+       }
+
+
+        public int Value
+        {
+            get { return _value; } 
+            set { _value = value;} 
+        } 
+
+        public void PrintMoney()
+        {
+            _printHelper.PrintMoney();
+        }
+
+        public void PrintNumber()
+        {
+            _printHelper.PrintNumber();
+        }
+
+   }
+
+
 
     class Program 
     {
 
         static void Main(string[] args) 
         {
-            Test test = new Test();
-            Print obj = test.printNumber;
-                  obj += test.printZero;  
-          
-            obj();
+            Number number = new Number(5);
+            number.PrintMoney();
+            number.PrintNumber();
         }
 
     }
