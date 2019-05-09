883a59e2cc17d4b34934bd3f9378e43ee8ed4b5e
anas1993 <shaikhanas1993@gmail.com>
2019-05-09 13:03:41 +0530

    learn about anonymous method in c#

--- Program.cs
+++ Program.cs
@@ -4,63 +4,94 @@ using Services;
 namespace c_
 {
     
-    /// <summary>
-    ///     Publisher Class
-    /// </summary>
-    class Publisher
-    {
-        public delegate void beforePrint(string message);
-        public event beforePrint beforePrintEvent;
+    // /// <summary>
+    // ///     Publisher Class
+    // /// </summary>
+    // class Publisher
+    // {
+    //     public delegate void beforePrint(string message);
+    //     public event beforePrint beforePrintEvent;
 
-        public Publisher(){}
+    //     public Publisher(){}
 
-        public void printNumber()
-        {
-            if(beforePrintEvent != null)
-            {
-                beforePrintEvent.Invoke("Print Number");
-            }
-        }
+    //     public void printNumber()
+    //     {
+    //         if(beforePrintEvent != null)
+    //         {
+    //             beforePrintEvent.Invoke("Print Number");
+    //         }
+    //     }
 
-    }
+    // }
 
-    class Subscriber
-    {
-       private Publisher _publisher;
+    // class Subscriber
+    // {
+    //    private Publisher _publisher;
 
-       public Subscriber()
-       {
-          _publisher = new Publisher();
-          _publisher.beforePrintEvent += myEventHandler;
-          _publisher.beforePrintEvent += myEventHandler1; 
-       } 
+    //    public Subscriber()
+    //    {
+    //       _publisher = new Publisher();
+    //       _publisher.beforePrintEvent += myEventHandler;
+    //       _publisher.beforePrintEvent += myEventHandler1; 
+    //    } 
 
-       public void myEventHandler(string message)
-       {
-           Console.WriteLine(message);
-       }
+    //    public void myEventHandler(string message)
+    //    {
+    //        Console.WriteLine(message);
+    //    }
 
-       public void myEventHandler1(string message)
-       {
-           Console.WriteLine(message);
-       }
+    //    public void myEventHandler1(string message)
+    //    {
+    //        Console.WriteLine(message);
+    //    }
 
-       public void PrintNumber()
-       {
-           _publisher.printNumber();
-       } 
+    //    public void PrintNumber()
+    //    {
+    //        _publisher.printNumber();
+    //    } 
        
 
-    }
+    // }
+
+
+    public delegate void Print(int value);
 
 
     class Program 
     {
+        // public delegate void Print();
+
+        // public delegate void PrintNumber(int number);
+
+        // public delegate void PrintLoop();
+        // const int  size = 10;
+
+        static void PrintHelperMethod(Print print,int value)
+        {
+            value = value + 10;
+            print(value);
+        }
 
         static void Main(string[] args) 
         {
-           var obj = new Subscriber();
-           obj.PrintNumber();
+            PrintHelperMethod(delegate(int value){ Console.WriteLine("value == {0}",value);},10);
+            
+        //     Print p = delegate(){
+        //         Console.WriteLine("hello world");
+        //     };
+
+        //     PrintNumber printNumber = delegate(int number){
+        //         Console.WriteLine("number :: {0}",number);
+        //     };
+
+        //     PrintLoop printLoop = delegate()
+        //     {
+        //         for(int i = 0; i< size ; i++){
+        //             Console.WriteLine(i);
+        //         }
+        //     };
+
+        //    printLoop();
         }
 
     }
